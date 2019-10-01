3b24b348677cf37bfd65ed28d52f729615394c76
Sumeet Sharma <sksksharma0@gmail.com>
2019-10-01 23:38:50 +0530

    Adding location filter

--- index.php
+++ index.php
@@ -7,12 +7,37 @@
 <?php 
   $sql = "SELECT * FROM internships WHERE deleted=0";
   $internships = $db->query($sql);
+  while($internship_filter = mysqli_fetch_assoc($internships)){
+    $category = $internship_filter['category'];
+    $location = $internship_filter['location'];
+    $nameOfCompany = $internship_filter['nameOfCompany'];
+    $duration = $internship_filter['duration'];
+  }
 ?>
 <main>
   <h2 class="text-center p-3">List of Internships</h2>
   <div class="container-fluid row">
     <div class="col-md-3">
       <h4 class="text-justify p-2">Filters</h4>
+      <form action="" method="post">
+
+        <h5 class="text-justify p-2">Location</h5>
+        <input type="radio" name="location" value="<?=$location;?>">
+
+        <button type="submit" name="search" class="btn btn-default">Search</button>
+
+      </form>
+
+
+
+      <?php 
+        if(isset($_POST['location']) && !empty($_POST['location'])){
+          if ($_POST['location'] == ''){
+            $sql .= " ORDER BY price ASC";
+            $products = $db->query($sql);
+          }  
+        }
+      ?>
     </div>
     <div class="col-md-9">
       <!-- List of Internships -->
